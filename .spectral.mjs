42a0d861119a7c21f9eea4b2741cf465989dd1d1
kannan.stealth@gmail.com <kannan.stealth@gmail.com>
2024-05-01 19:43:13 +0530

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI0NDcxNw/spectral.js?branch_slug=main&token=c18076e0-2b36-4707-8de0-306d40f162f6";
+export default { extends: ruleset };
